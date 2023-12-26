07bde3ce7a3a512a160369b70866296c91063e93
Samson Ameh <amehsam69@gmail.com>
2023-12-26 21:14:09 +0100

    added all functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,60 @@
+import { useEffect, useState } from "react";
 import QuoteCard from "./component/QuoteCard";
 
 export default function App() {
+  const [quotes, setQuotes] = useState([]);
+  const [selectedQuote, setSelectedQuote] = useState({});
+  const [backgroundColor, setBackgroundColor] = useState("");
+
+  const colors = [
+    "#16a085",
+    "#27ae60",
+    "#2c3e50",
+    "#f39c12",
+    "#e74c3c",
+    "#9b59b6",
+    "#FB6964",
+    "#342224",
+    "#472E32",
+    "#BDBB99",
+    "#77B1A9",
+    "#73A857",
+  ];
+
+  useEffect(() => {
+    fetch(
+      "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setQuotes(data.quotes);
+        const randomIndex = Math.floor(Math.random() * data.quotes.length);
+        setSelectedQuote(data.quotes[randomIndex]);
+      });
+  }, []);
+
+  function getRandomColor() {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
+  }
+
+  const getRandomQuote = () => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    setSelectedQuote(quotes[randomIndex]);
+    const randomColor = getRandomColor();
+    setBackgroundColor(randomColor);
+
+    document.body.style.transition = "background-color 0.5s ease";
+    document.body.style.backgroundColor = randomColor;
+  };
+
   return (
-    <>
-      <QuoteCard />
-    </>
+    <div>
+      <QuoteCard
+        quote={selectedQuote}
+        onButtonClick={getRandomQuote}
+        bgColor={backgroundColor}
+      />
+    </div>
   );
 }
--- src/component/QuoteCard.jsx
+++ src/component/QuoteCard.jsx
@@ -1,21 +1,31 @@
 import { FaQuoteLeft, FaXTwitter, FaFacebookF } from "react-icons/fa6";
 
-export default function QuoteCard() {
+export default function QuoteCard({ quote, onButtonClick, bgColor }) {
   return (
     <div className="quoteCard-container">
       <blockquote>
         <p>
           <FaQuoteLeft className="quote-icon" />
-          <span className="quote">
-            Your actual income is not what you get, but what you give.
-          </span>
+          <span className="quote">{quote.quote}</span>
         </p>
-        <footer className="author">- Author Name</footer>
+        <footer className="author">- {quote.author}</footer>
       </blockquote>
       <div className="quote-share">
-        <FaXTwitter className="share-icon" />
-        <FaFacebookF className="share-icon" />
-        <button className="new-quote-btn">New quote</button>
+        <FaXTwitter
+          className="share-icon"
+          style={{ backgroundColor: bgColor }}
+        />
+        <FaFacebookF
+          className="share-icon"
+          style={{ backgroundColor: bgColor }}
+        />
+        <button
+          className="new-quote-btn"
+          onClick={onButtonClick}
+          style={{ backgroundColor: bgColor }}
+        >
+          New quote
+        </button>
       </div>
     </div>
   );
