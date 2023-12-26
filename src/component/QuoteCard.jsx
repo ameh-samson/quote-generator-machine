e662e9344471a2eedd148a7c4028ac27f869475b
Samson Ameh <amehsam69@gmail.com>
2023-12-26 21:19:18 +0100

    random color added to the quote

--- src/component/QuoteCard.jsx
+++ src/component/QuoteCard.jsx
@@ -3,7 +3,7 @@ import { FaQuoteLeft, FaXTwitter, FaFacebookF } from "react-icons/fa6";
 export default function QuoteCard({ quote, onButtonClick, bgColor }) {
   return (
     <div className="quoteCard-container">
-      <blockquote>
+      <blockquote style={{ color: bgColor }}>
         <p>
           <FaQuoteLeft className="quote-icon" />
           <span className="quote">{quote.quote}</span>
